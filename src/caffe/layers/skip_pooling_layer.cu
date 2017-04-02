81919c6904b7d33cc9efa4dc8376632e7ea9c054
Hongliang Yuan <fallingdust@gmail.com>
2017-04-02 18:23:18 +0800

    Fix bug of SkipPoolingLayer.

--- src/caffe/layers/skip_pooling_layer.cu
+++ src/caffe/layers/skip_pooling_layer.cu
@@ -11,28 +11,18 @@ template <typename Dtype>
 __global__ void SkipPoolForward(const int nthreads,
     const Dtype* const bottom_data, const int num, const int channels,
     const int height, const int width, const int pooled_height,
-    const int pooled_width, const int kernel_h, const int kernel_w,
-    const int stride_h, const int stride_w, const int pad_h, const int pad_w,
-    Dtype* const top_data) {
+    const int pooled_width, Dtype* const top_data) {
   CUDA_KERNEL_LOOP(index, nthreads) {
     const int pw = index % pooled_width;
     const int ph = (index / pooled_width) % pooled_height;
     const int c = (index / pooled_width / pooled_height) % (channels * 4);
     const int n = index / pooled_width / pooled_height / (channels * 4);
-    int hstart = ph * stride_h - pad_h;
-    int wstart = pw * stride_w - pad_w;
-    const int hend = min(hstart + kernel_h, height);
-    const int wend = min(wstart + kernel_w, width);
-    hstart = max(hstart, 0);
-    wstart = max(wstart, 0);
-    top_data[index] = Dtype(0);
+    int hstart = ph * 2;
+    int wstart = pw * 2;
     const Dtype* const bottom_slice =
         bottom_data + (n * channels + c / 4) * height * width;
-    for (int h = hstart; h < hend; ++h) {
-      for (int w = wstart; w < wend; ++w) {
-        top_data[index] = bottom_slice[h * width + w];
-      }
-    }
+    int bottom_index = (hstart + c % 4 / 2) * width + wstart + c % 4 % 2;
+    top_data[index] = bottom_index > height * width ? Dtype(0) : bottom_slice[bottom_index];
   }
 }
 
@@ -46,8 +36,7 @@ void SkipPoolingLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
   // NOLINT_NEXT_LINE(whitespace/operators)
   SkipPoolForward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
       count, bottom_data, bottom[0]->num(), channels_,
-      height_, width_, pooled_height_, pooled_width_, 2,
-      2, 2, 2, 0, 0, top_data);
+      height_, width_, pooled_height_, pooled_width_, top_data);
 
   CUDA_POST_KERNEL_CHECK;
 }
